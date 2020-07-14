2ff467f4f63239a01a37c155a8bcae27502923dc
Nathalie <nathaliemekahal@gmail.com>
2020-07-14 16:06:59 +0200

    posting projects for a specific student id

--- services/routes/projects/index.js
+++ services/routes/projects/index.js
@@ -5,13 +5,13 @@ const router=express.Router()
 
 
 
-router.post("/", async (req, res)=> {
+router.post("/:studentId", async (req, res)=> {
     const response =
     await db.query(`INSERT INTO 
     "Projects" (name, description, "creationDate", "repoUrl","liveUrl", "studentId", "proj_id")                                    
     Values ($1, $2, $3, $4, $5,$6,$7)
     RETURNING *`, 
-    [ req.body.name, req.body.description, req.body.creationDate,req.body.repoUrl, req.body.liveUrl, req.body.studentId,req.body.proj_id ])
+    [ req.body.name, req.body.description, req.body.creationDate,req.body.repoUrl, req.body.liveUrl, req.params.studentId,req.body.proj_id ])
     console.log(response)
     res.send(response.rows[0])
 })
